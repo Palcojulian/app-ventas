319b71960abd7de40329b100d6ab27236eb9be21
palcojulian <palcojulian@outlook.com>
2026-05-12 16:08:39 -0500

    feat: Remove 'codigo' generation logic from UpdateProductoAction

--- app/Actions/Productos/UpdateProductoAction.php
+++ app/Actions/Productos/UpdateProductoAction.php
@@ -9,11 +9,7 @@ class UpdateProductoAction
 {
     public function handle(Producto $producto, array $data): Producto
     {
-        $productos_creados = count(Producto::where("id_categoria", "=", $data['id_categoria'])->get()) + 1;
-        $prefijo = Categoria::find($data['id_categoria'])->prefijo;
-
         $producto->update([
-            'codigo' => "{$prefijo}-" . str_pad($productos_creados, 4, '0', STR_PAD_LEFT),
             'nombre' => $data['nombre'] ?? $producto->nombre,
             'descripcion' => $data['descripcion'] ?? $producto->descripcion,
             'id_categoria' => $data['id_categoria'] ?? $producto->id_categoria,
@@ -24,10 +20,7 @@ class UpdateProductoAction
             'unidad_medida' => $data['unidad_medida'] ?? $producto->unidad_medida,
             'estado' => $data['estado'] ?? $producto->estado,
         ]);
-
-    
-
-
+        
         return $producto->fresh();
     }
 }
