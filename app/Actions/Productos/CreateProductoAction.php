a1eda523dd752081c5647c7da214f2815ac3cd61
palcojulian <palcojulian@outlook.com>
2026-06-11 21:46:59 -0500

    feat: :recycle: Create product

--- app/Actions/Productos/CreateProductoAction.php
+++ app/Actions/Productos/CreateProductoAction.php
@@ -12,18 +12,8 @@ class CreateProductoAction
     {
         $productos_creados = count(Producto::where("id_categoria", "=", $data['id_categoria'])->get()) + 1;
         $prefijo = Categoria::find($data['id_categoria'])->prefijo;
-
-        return Producto::create([
-            'codigo' => "{$prefijo}-" . str_pad($productos_creados, 4, '0', STR_PAD_LEFT),
-            'nombre' => $data['nombre'],
-            'descripcion' => $data['descripcion'] ?? null,
-            'id_categoria' => $data['id_categoria'],
-            'precio_venta' => $data['precio_venta'],
-            'costo' => $data['costo'] ?? 0,
-            'stock_actual' => $data['stock_actual'] ?? 0,
-            'stock_minimo' => $data['stock_minimo'] ?? 0,
-            'unidad_medida' => $data['unidad_medida'] ?? null,
-            'estado' => $data['estado'] ?? 'activo',
-        ])->refresh();
+        $data['codigo'] = "{$prefijo}-" . str_pad($productos_creados, 4, '0', STR_PAD_LEFT);
+        $data['estado'] =  $data['estado'] ?? 'activo';
+        return Producto::create($data)->refresh();
     }
 }
